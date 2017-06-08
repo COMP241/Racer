58c8c34d5c49acd1380f1f1f5124f3cfe3232efa
Sivaram Manoharan <sivaramigcse@gmail.com>
2017-06-08 20:51:08 +1200

    Allows car to move
    Lets the car move after the timer is up

--- Assets/Scripts/SetCountdown.cs
+++ Assets/Scripts/SetCountdown.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class SetCountdown : MonoBehaviour
 {
+    public Player player;
+    private Rigidbody rb;
     public void SetCountedDown(){
+        player = GameObject.Find("Car").GetComponent<Player>();
+        rb = player.GetComponent<Rigidbody>();
+        rb.isKinematic = false;
         gameController.EnableCountedDown();
     }
 }
